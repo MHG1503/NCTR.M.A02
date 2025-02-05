23278bedb116f2322e380dad51e846ec23d78122
= <=>
2025-02-05 16:38:18 +0700

    commit EX01 02

---
+++ NCTR.M.A02.Ex01/Program.cs
@@ -0,0 +1,68 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string taskName;
+        DateTime taskTime;
+
+        while (true){
+            System.Console.WriteLine("Enter the task name: ");
+            taskName = Console.ReadLine();
+            if (taskName != "" && taskName != null){
+                break;
+            }
+        }
+
+        string[] formats = {"dd/MM/yyyy", "yyyy/MM/dd"};
+        while (true){
+            System.Console.WriteLine("Enter the task time (dd/MM/yyyy or yyyy/MM/dd): ");
+            if(DateTime.TryParseExact(
+                Console.ReadLine(),
+                formats,
+                System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None,out taskTime)
+            ){
+                break;
+            }
+        }
+
+        PrintResult(taskName,taskTime, GenerateReminders(taskTime));
+    }
+
+    private static List<DateTime> GenerateReminders(DateTime taskTime){
+        List<DateTime> result = new List<DateTime>();
+        int count = 1;
+        while(count <= 5 ){
+            if(count == 1){
+                taskTime = addWorkingDays(taskTime,7);
+            }else if(count == 2){
+                taskTime = addWorkingDays(taskTime,2);
+            }else{
+                taskTime = addWorkingDays(taskTime,1);
+            }
+            result.Add(taskTime);
+            count++;
+        }
+        return result;
+    }
+
+    private static DateTime addWorkingDays(DateTime taskTime, int days){
+        while(days > 0){
+            taskTime = taskTime.AddDays(1);
+            if(!IsWeekend(taskTime)){
+                days--;
+            }
+        }
+        return taskTime;
+    }
+
+    private static bool IsWeekend(DateTime currentDate){
+        return currentDate.DayOfWeek == DayOfWeek.Sunday || currentDate.DayOfWeek == DayOfWeek.Saturday;
+    }
+
+    private static void PrintResult(string taskName, DateTime taskTime, List<DateTime> remindedTime){
+        foreach (DateTime time in remindedTime){
+            System.Console.WriteLine($"Task: {taskName} created on {taskTime:yyyy/MM/dd} - Reminder: {time:yyyy/MM/dd}");
+        }
+    }
+}
---
+++ NCTR.M.A02.Ex02/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int personCount;
+        while(true){
+            System.Console.WriteLine("Enter the number of person");
+            if(int.TryParse(Console.ReadLine(), out personCount)){
+                break;
+            }
+        }
+        string[] persons = new string[personCount];
+        string[] formats = {"dd/MM/yyyy", "yyyy/MM/dd"};
+
+        System.Console.WriteLine("Enter the names and birthdates in the format: Name - dd/MM/yyyy");
+        int count = 1;
+
+        while(count <= personCount){
+            System.Console.WriteLine($"Enter the name and birthdate of person {count}");
+            string infoInput = Console.ReadLine();
+
+            if(infoInput != "" && infoInput != null){
+                try{
+                    string[] splitInfo = infoInput.Trim().Split(" - ");
+                    string name = splitInfo[0];
+                    string birthDateStr = splitInfo[1];
+                    DateTime birthDate;
+
+                    if(!(splitInfo[0] != "" && 
+                        DateTime.TryParseExact(
+                            birthDateStr,
+                            formats,
+                            System.Globalization.CultureInfo.InvariantCulture,
+                            System.Globalization.DateTimeStyles.None,out birthDate)))
+                    {
+                        System.Console.WriteLine("Invalid Input for Birthdate");
+                        continue;
+                    }
+                    persons[count - 1] = $"{name} - {birthDate:yyyy/MM/dd}";
+                    count++;
+                }catch(Exception e){
+                    System.Console.WriteLine("Invalid Input. Please enter again");
+                    continue;
+                }
+            }
+        }
+
+        for(int i = 0; i < personCount; i++){
+            System.Console.WriteLine($"Person {i + 1}: {persons[i]}");
+        }  
+    }
+}
